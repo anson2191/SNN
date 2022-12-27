105dc715ed2439ed1e3eb5016d6ecf9ed93d8a20
anson2191 <anson6799@gmail.com>
2022-12-27 17:23:55 +0800

    1227

---
+++ src/SNN/PE.sv
@@ -0,0 +1,15 @@
+module PE(
+	input clk,
+	input rst,
+	input SPIKE_in0,
+	input SPIKE_in1,
+	input SPIKE_in2,
+	input SPIKE_in3,
+	input [7:0] WEIGHT_IN,
+	input EN,
+	input layer,
+	output SPIKE_OUT
+);
+
+
+endmodule
